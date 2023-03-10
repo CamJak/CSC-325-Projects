7122e6ca9c090daacaacbe44cbf96b6460c1129c
CamJak <cameron102601@gmail.com>
2023-03-10 12:09:24 -0600

    Implemented program 0

--- Program_0/classes.py
+++ Program_0/classes.py
@@ -1,14 +1,73 @@
 from random import randint
 
+# Simple node class
+class node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.link = None
+
+    # Acessors
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def link(self):
+        return self._link
+    
+    # Mutators
+    @data.setter
+    def data(self, data):
+        self._data = data
+
+    @link.setter
+    def link(self, link):
+        self._link = link
+
+
+# Linked list class
 class linkedList:
     def __init__(self, num_nodes) -> None:
-        self.listNodes = []
-        link = 0
-        for x in range(num_nodes-1):
-            newNode = node(randint(0,100), link)
-            self.listNodes.append()
+        self.length = num_nodes
+        self.head = node(randint(0,100))
+        self.prev = self.head
 
-class node:
-    def __init__(self, data, link) -> None:
-        self.data = data
-        self.link = link
+        # Generate linked list
+        # Data is a random number from 0 to 100
+        for i in range(num_nodes-1):
+            newNode = node(randint(0,100))
+            self.prev.link = newNode
+            self.prev = newNode
+
+        self.tail = self.prev
+
+    # Returns string version of linked list
+    def __str__(self) -> str:
+        str_out = ""
+        currNode = self.head
+        for i in range(self.length):
+            str_out = str_out + str(currNode.data) + " "
+            currNode = currNode.link
+        return str_out
+    
+    # Function to return first node containing matching data
+    def find(self, currNode, data) -> node:
+        while (currNode.data != data):
+            currNode = currNode.link
+        return currNode
+
+    # Function to sort linked list using selection sort
+    # Checks each node against all nodes to its right and swaps with minimum data node
+    def sort(self) -> None:
+        index = 1
+        currNode = self.head
+        while (index < self.length):
+            minData = currNode.data
+            peekNode = currNode.link
+            while (peekNode != None):
+                minData = min(peekNode.data, minData)
+                peekNode = peekNode.link
+            self.find(currNode, minData).data = currNode.data
+            currNode.data = minData
+            currNode = currNode.link
+            index += 1
--- Program_0/main.py
+++ Program_0/main.py
@@ -1,6 +1,30 @@
 from classes import linkedList
 
-num_nodes = int(input("Please, enter the number of nodes: "))
+# Request number of nodes from user
+request_string = "Please, enter the number of nodes: "
+while True:
+    try:
+        num_nodes = int(input(request_string))
+        # Raise exception if number is less than 1 (does not make sense for linked list)
+        if (num_nodes < 1):
+            raise Exception
+        break
+    except Exception:
+        # If incorrect input, print newline and change request message
+        print()
+        request_string = "Please, enter correct value for number of nodes: "
 
+
+# Create linked list based on user input
 newList = linkedList(num_nodes)
-print(newList)
+# Output information about newly created list
+print("Unsorted list: " + str(newList))
+print("Head data: {}".format(newList.head.data))
+print("Tail data: {}\n".format(newList.tail.data))
+
+# Sort list
+newList.sort()
+# Output sorted list information
+print("Sorted list: " + str(newList))
+print("Head data: {}".format(newList.head.data))
+print("Tail data: {}\n".format(newList.tail.data))
